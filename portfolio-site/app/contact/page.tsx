adcc54ba5687c4c0d6beceec4ff2b2060a4d109c
Jed Harwood <jed.harwood@appliedsystems.com>
2025-07-02 10:48:18 +0100

    Blanked other pages to test routing

---
+++ portfolio-site/app/contact/page.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import Navbar from '../components/nav-bar';
+import Image from 'next/image';
+import hakkei from '../../public/hakkei-lanterns.jpg';
+
+const ContactPage: FC = () => {
+  return (
+    <main className="px-0 lg:px-16 xl:px-48 2xl:px-64">
+      <div className='relative'>
+          <Image
+              src={hakkei}
+              alt='Lanterns outside Hakkei'
+              priority
+              className='w-full shadow-lg'
+              placeholder='blur'
+              width='500'
+              height='100'
+          />
+      </div>
+      <Navbar />
+      <div className='px-4 lg:px-0'>
+          <h1 className="text-4xl font-bold">Contact Page</h1>
+      </div>
+      <div>FOOTER</div>
+    </main>
+  );
+}
+
+export default ContactPage;
