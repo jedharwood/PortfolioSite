197843ce8b2f753544d75fc22d9bbb476892486e
Jed Harwood <jed.harwood@appliedsystems.com>
2025-07-02 17:51:14 +0100

    Finished Footer

--- portfolio-site/app/components/footer.tsx
+++ portfolio-site/app/components/footer.tsx
@@ -1,23 +1,30 @@
+'use client';
 import { JSX } from 'react';
-// import { SvgButton } from "./svg-button";
+import { SvgButton } from './svg-button';
 import { SvgAnchor } from './svg-anchor';
 
-export const Footer = (): JSX.Element => {
-    // scroll to top button
-
-    return (
-        <footer className='flex w-full justify-center p-2'>
-            {/* <SvgButton onClickFunction={() => window.scrollTo(0, 0)} screenReaderText="link to LinkedIn page" buttonType="burger-menu"/> */}
-            <SvgAnchor
-                label='Link to LinkedIn page'
-                href='https://uk.linkedin.com/in/jed-harwood-39aaba194'
-                buttonType='linked-in'
-            />
-            <SvgAnchor
-                label='Link to github page'
-                href='https://github.com/jedharwood'
-                buttonType='github'
-            />
-        </footer>
-    );
+const scrollToTop = (): void => {
+    if (typeof window === 'undefined') return;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 };
+
+export const Footer = (): JSX.Element => (
+    <footer className='py-4 px-2 flex w-full justify-center gap-2'>
+        <SvgAnchor
+            label='Link to LinkedIn page'
+            href='https://uk.linkedin.com/in/jed-harwood-39aaba194'
+            buttonType='linked-in'
+        />
+        <SvgAnchor
+            label='Link to github page'
+            href='https://github.com/jedharwood'
+            buttonType='github'
+        />
+        <SvgButton
+            onClickFunction={scrollToTop}
+            label='Scroll to top'
+            buttonType='up-chevron'
+            size='large'
+        />
+    </footer>
+);
