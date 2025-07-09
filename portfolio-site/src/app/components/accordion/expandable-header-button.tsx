041ceaf73be8a6350a608afa4b55361133413e96
Jed Harwood <scaryteeth@hotmail.com>
2025-07-09 20:00:37 +0100

    Consolidated svg and header into button component

--- portfolio-site/src/app/components/accordion/expandable-header-button.tsx
+++ portfolio-site/src/app/components/accordion/expandable-header-button.tsx
@@ -1,6 +1,4 @@
 import { JSX } from 'react';
-import { AccordionHeader } from './accordion-header';
-import { ExpandAccordionSvg } from './expand-accordion-svg';
 
 type ExpandableHeaderButtonProps = {
     id: string;
@@ -18,6 +16,46 @@ export const ExpandableHeaderButton = ({
     isNestedAccordion = false,
 }: ExpandableHeaderButtonProps): JSX.Element => {
     const expandableHeaderButtonClassNames: string = `flex w-full items-center justify-between text-left${isNestedAccordion && ' py-2'}`;
+    const {jobTitle, companyName, location, dateRange} = headerProps;
+
+    const accordionHeader: JSX.Element = (
+        <div className='flex w-full flex-col gap-1 font-semibold'>
+            <div className='flex w-full flex-wrap space-x-2'>
+                <span className='whitespace-nowrap'>{`${jobTitle},`}</span>
+                <span className='whitespace-nowrap'>{companyName}</span>
+            </div>
+            <div className='flex w-full flex-wrap justify-between space-x-4 text-sm'>
+                <span className='whitespace-nowrap'>{location}</span>
+                <span className='whitespace-nowrap'>{dateRange}</span>
+            </div>
+        </div>
+    );
+
+    const rectClassNames: string = `origin-center transform transition duration-200 ease-out ${accordionIsOpen && '!rotate-180'}`;
+    const expandAccordionSvg: JSX.Element = (
+        <svg
+            className='svg-button ml-4 shrink-0 lg:ml-6'
+            width='16'
+            height='16'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='currentColor'
+        >
+            <rect
+                y='7'
+                width='20'
+                height='2'
+                rx='1'
+                className={rectClassNames}
+            />
+            <rect
+                y='7'
+                width='20'
+                height='2'
+                rx='1'
+                className={`${rectClassNames} rotate-90`}
+            />
+        </svg>
+    );
 
     return (
         <button
@@ -26,8 +64,8 @@ export const ExpandableHeaderButton = ({
             aria-expanded={accordionIsOpen}
             aria-controls={id}
         >
-            <AccordionHeader {...headerProps} />
-            <ExpandAccordionSvg accordionIsOpen={accordionIsOpen} />
+            {accordionHeader}
+            {expandAccordionSvg}
         </button>
     );
 };
