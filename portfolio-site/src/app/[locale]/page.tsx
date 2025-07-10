13b56889c234e89017c0a54a5298bf0eb2d564f0
Jed Harwood <scaryteeth@hotmail.com>
2025-07-10 14:44:29 +0100

    Removed unused fragments

--- portfolio-site/src/app/[locale]/page.tsx
+++ portfolio-site/src/app/[locale]/page.tsx
@@ -15,19 +15,17 @@ const HomePage: FC = () => {
     const t = useTranslations('Home');
 
     return (
-        <>
-            <div className='px-4 lg:px-0'>
-                <h1 className='text-4xl font-bold'>Jed Harwood</h1>
-                <h2 className='text-lg text-gray-600'>Software Engineer</h2>
-                <h3>
-                    Full stack software engineer with a passion for creating
-                    great, accessible user experiences with ReactJs and other
-                    modern JavaScript frameworks
-                </h3>
-                <h1>{t('title')}</h1>
-                {generatePageContent()}
-            </div>
-        </>
+        <div className='px-4 lg:px-0'>
+            <h1 className='text-4xl font-bold'>Jed Harwood</h1>
+            <h2 className='text-lg text-gray-600'>Software Engineer</h2>
+            <h3>
+                Full stack software engineer with a passion for creating
+                great, accessible user experiences with ReactJs and other
+                modern JavaScript frameworks
+            </h3>
+            <h1>{t('title')}</h1>
+            {generatePageContent()}
+        </div>
     );
 };
 
--- portfolio-site/src/app/[locale]/projects/page.tsx
+++ portfolio-site/src/app/[locale]/projects/page.tsx
@@ -2,11 +2,9 @@ import { FC } from 'react';
 
 const ProjectsPage: FC = () => {
     return (
-        <>
-            <div className='px-4 lg:px-0'>
-                <h1 className='text-4xl font-bold'>Projects Page</h1>
-            </div>
-        </>
+        <div className='px-4 lg:px-0'>
+            <h1 className='text-4xl font-bold'>Projects Page</h1>
+        </div>
     );
 };
 
