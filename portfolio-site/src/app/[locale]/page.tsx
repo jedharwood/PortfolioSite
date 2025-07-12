10208776cec52db33a27c4ca1eee225e72277c79
Jed Harwood <scaryteeth@hotmail.com>
2025-07-12 14:58:32 +0100

    Page content WIP

--- portfolio-site/src/app/[locale]/page.tsx
+++ portfolio-site/src/app/[locale]/page.tsx
@@ -1,30 +1,36 @@
 import { FC } from 'react';
 import { useTranslations } from 'next-intl';
-
-const generatePageContent = () => {
-    return (
-        <ul>
-            {Array.from({ length: 100 }, (_, index) => (
-                <li key={index}>content</li>
-            ))}
-        </ul>
-    );
-};
+import SvgIcon from '../components/svg-icon';
 
 const HomePage: FC = () => {
     const t = useTranslations('Home');
 
     return (
         <div className='px-4 lg:px-0'>
-            <h1 className='text-4xl font-bold'>Jed Harwood</h1>
-            <h2 className='text-lg text-gray-600'>Software Engineer</h2>
-            <h3>
-                Full stack software engineer with a passion for creating
-                great, accessible user experiences with ReactJs and other
-                modern JavaScript frameworks
-            </h3>
-            <h1>{t('title')}</h1>
-            {generatePageContent()}
+            <h1 className='text-4xl font-bold'>{t('title')}</h1>
+            <h2 className='text-lg'>{t('subTitle')}</h2>
+            <p>{t('paragraphOne')}</p>
+            <p>{t('paragraphTwo')}</p>
+            <p>{t('paragraphThree')}</p>
+            <p>{t('experiencedWith')}</p>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
+                <div className='space-y-2 flex flex-col'>
+                    <SvgIcon text={'Typescript'} />
+                    <SvgIcon text={'Javascript'} />
+                    <SvgIcon text={'NodeJs'} />
+                    <SvgIcon text={'Fastify'} />
+                </div>
+                <div className='space-y-2 flex flex-col'>
+                    <SvgIcon text={'ReactJs'} />
+                    <SvgIcon text={'VueJs'} />
+                    <SvgIcon text={'.Net'} />
+                </div>
+                <div className='space-y-2 flex flex-col'>
+                    <SvgIcon text={'Rails'} />
+                    <SvgIcon text={'MongoDb'} />
+                    <SvgIcon text={'MySql'} />
+                </div>
+            </div>
         </div>
     );
 };
