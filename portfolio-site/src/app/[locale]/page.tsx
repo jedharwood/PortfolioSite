395e4b3e00a04feb5e75955b5c43e1e237a1ae91
Jed Harwood <scaryteeth@hotmail.com>
2025-07-14 09:47:44 +0100

    Refactor SVGIcon

--- portfolio-site/src/app/[locale]/page.tsx
+++ portfolio-site/src/app/[locale]/page.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 import { useTranslations } from 'next-intl';
-import SvgIcon from '../components/svg-icon';
+import SvgIcon from '../components/svg-icon/svg-icon';
 
 const HomePage: FC = () => {
     const t = useTranslations('Home');
+    // might need to translate the aria labels
 
     return (
         <div className='px-4 lg:px-0'>
@@ -13,45 +14,62 @@ const HomePage: FC = () => {
             <p>{t('paragraphTwo')}</p>
             <p>{t('paragraphThree')}</p>
             <p>{t('experiencedWith')}</p>
-            <div className='grid grid-cols-1 gap-6 lg:grid-cols-3'>
-                <div className='flex flex-col space-y-2'>
+            <div className='grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                <section
+                    className='flex flex-col space-y-2'
+                    aria-label='Frontend frameworks'
+                >
+                    <SvgIcon type='react' />
+                    <SvgIcon type='next' />
                     <SvgIcon type='typescript' />
                     <SvgIcon type='javascript' />
-                    <SvgIcon type='react' />
                     <SvgIcon type='vue' />
-                    <SvgIcon type='next' />
                     <SvgIcon type='html' />
-                </div>
-                <div className='flex flex-col space-y-2'>
-                    <SvgIcon type='node' />
-                    <SvgIcon type='dotnet' />
-                    <SvgIcon type='fastify' />
-                    <SvgIcon type='rails' />
-                </div>
-                <div className='flex flex-col space-y-2'>
+                </section>
+                <section
+                    className='flex flex-col space-y-2'
+                    aria-label='Styling libraries'
+                >
                     <SvgIcon type='css' />
+                    <SvgIcon type='styledcomponents' />
                     <SvgIcon type='tailwind' />
                     <SvgIcon type='bootstrap' />
                     <SvgIcon type='sass' />
                     <SvgIcon type='less' />
-                    <SvgIcon type='styledcomponents' />
-                </div>
-                <div className='flex flex-col space-y-2'>
+                </section>
+                <section
+                    className='flex flex-col space-y-2'
+                    aria-label='Database technologies'
+                >
                     <SvgIcon type='sequelize' />
-                    <SvgIcon type='mongo' />
                     <SvgIcon type='mysql' />
-                    <SvgIcon type='tsql' />
+                    <SvgIcon type='mongo' />
                     <SvgIcon type='postgres' />
-                </div>
-                <div className='flex flex-col space-y-2'>
+                    <SvgIcon type='tsql' />
+                </section>
+                <section
+                    className='flex flex-col space-y-2'
+                    aria-label='Backend frameworks'
+                >
+                    <SvgIcon type='node' />
+                    <SvgIcon type='dotnet' />
+                    <SvgIcon type='fastify' />
+                    <SvgIcon type='rails' />
+                </section>
+                <section
+                    className='flex flex-col space-y-2'
+                    aria-label='Design considerations'
+                >
                     <SvgIcon type='a11y' />
                     <SvgIcon type='responsivedesign' />
-                </div>
-
-                <div className='flex flex-col space-y-2'>
+                </section>
+                <section
+                    className='flex flex-col space-y-2'
+                    aria-label='End-to-end testing libraries'
+                >
                     <SvgIcon type='cypress' />
                     <SvgIcon type='specflow' />
-                </div>
+                </section>
             </div>
         </div>
     );
