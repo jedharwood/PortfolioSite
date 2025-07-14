fd8650c31a89e984b919a5ff3b6f9d9f99159108
Jed Harwood <scaryteeth@hotmail.com>
2025-07-14 14:07:05 +0100

    Tidy up other SVG components

--- portfolio-site/src/app/components/svg-button.tsx
+++ portfolio-site/src/app/components/svg-button.tsx
@@ -1,5 +1,34 @@
 import { JSX } from 'react';
 
+const burgerMenuSvg: JSX.Element = (
+    <svg
+        className='h-12 w-12'
+        aria-hidden='true'
+        focusable='false'
+        fill='currentColor'
+        viewBox='0 0 17 14'
+    >
+        <path
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeWidth='2'
+            d='M1 1h15M1 7h15M1 13h15'
+        />
+    </svg>
+);
+
+const upChevronSvg: JSX.Element = (
+    <svg
+        className='h-20 w-20'
+        aria-hidden='true'
+        focusable='false'
+        viewBox='0 0 24 24'
+        fill='none'
+    >
+        <path d='M6 15L12 9L18 15' stroke='currentColor' strokeWidth='2' />
+    </svg>
+);
+
 type SvgButtonProps = {
     onClickFunction: () => void;
     label: string;
@@ -22,39 +51,7 @@ const SvgButton = ({
             aria-label={label}
             onClick={onClickFunction}
         >
-            {buttonType === 'burger-menu' ? (
-                <svg
-                    className='h-12 w-12'
-                    aria-hidden='true'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='currentColor'
-                    viewBox='0 0 17 14'
-                >
-                    <path
-                        stroke='currentColor'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        strokeWidth='2'
-                        d='M1 1h15M1 7h15M1 13h15'
-                    />
-                </svg>
-            ) : (
-                <svg
-                    className='h-20 w-20'
-                    aria-hidden='true'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'
-                >
-                    <path
-                        d='M6 15L12 9L18 15'
-                        stroke='currentColor'
-                        strokeWidth='2'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                    />
-                </svg>
-            )}
+            {buttonType === 'burger-menu' ? burgerMenuSvg : upChevronSvg}
         </button>
     );
 };
