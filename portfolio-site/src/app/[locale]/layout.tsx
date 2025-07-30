a2526346860e7523291119b35c7e6f58982f8317
Jed Harwood <scaryteeth@hotmail.com>
2025-07-30 12:30:01 +0100

    Stripped back to just favicon

--- portfolio-site/src/app/[locale]/layout.tsx
+++ portfolio-site/src/app/[locale]/layout.tsx
@@ -24,14 +24,8 @@ export const metadata: Metadata = {
     icons: {
         icon: [
             {
-                media: '(prefers-color-scheme: light)',
-                url: '/favicon-light.svg',
-                href: '/favicon-light.svg',
-            },
-            {
-                media: '(prefers-color-scheme: dark)',
-                url: '/favicon-dark.svg',
-                href: '/favicon-dark.svg',
+                url: '/favicon.ico',
+                type: 'image/x-icon',
             },
         ],
         apple: '/apple-touch-icon.png',
