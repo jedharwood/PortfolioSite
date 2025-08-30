7a79b249041a7afb476243e8699389c1e0ea6e1c
Jed Harwood <scaryteeth@hotmail.com>
2025-08-30 17:59:11 +0100

    Completed navigation tests

--- portfolio-site/cypress/e2e/navigation.cy.ts
+++ portfolio-site/cypress/e2e/navigation.cy.ts
@@ -1,49 +1,99 @@
-const openNavMenuLocaleEn = (): void => {
+export const locales = {
+  en: 'en',
+  ja: 'ja',
+} as const
+
+export type Locale = keyof typeof locales
+
+const openNavMenuWithLocale = (locale: Locale): void => {
     cy.get('button[aria-label="Open main menu"]').click();
     cy.get('#burger-menu').within(() => {
-        cy.contains('a', /^Home$/).should('be.visible');
-        cy.contains('a', /^Work History$/).should('be.visible');
-        cy.contains('a', /^Projects$/).should('be.visible');
-        cy.contains('a', /^Contact$/).should('be.visible');
+        cy.contains(`a[href*="/${locale}"]`, /^Home$/).should('be.visible');
+        cy.contains(`a[href*="/${locale}/work-history"]`, /^Work History$/).should(
+            'be.visible',
+        );
+        cy.contains(`a[href*="/${locale}/projects"]`, /^Projects$/).should(
+            'be.visible',
+        );
+        cy.contains(`a[href*="/${locale}/contact"]`, /^Contact$/).should('be.visible');
     });
 };
 
-const visitHomePageLocaleEn = (): void => {
+const visitHomePageWithLocale = (locale: Locale): void => {
     cy.visit('http://localhost:3000');
-    // cy.location('pathname').should('eq', '/en');
-    cy.url().should('equal', 'http://localhost:3000/en');
+
+    cy.get('h1').contains('Jed Harwood');
+    cy.url().should('equal', `http://localhost:3000/${locale}`);
+    cy.get('#burger-menu')
+        .find(`a[href*="/${locale}"]`)
+        .should('have.css', 'text-decoration-line', 'underline')
 };
 
-const findAndClickNavLink = (navLink: string) => {
+const findAndClickNavLink = (locale: Locale, path: string, expectedTextContent: string) => {
     cy.get('#burger-menu')
-            .find(`a[href*="/${navLink}"]`)
-            .should('be.visible')
-            .click();
+        .find(`a[href*="/${locale}/${path}"]`)
+        .should('be.visible')
+        .click();
 
-    // cy.location('pathname').should('eq', '/en/contact');
-    cy.url().should('equal', `http://localhost:3000/${navLink}`);
-}
+    cy.get('h1').contains(expectedTextContent);
+    cy.url().should('equal', `http://localhost:3000/${locale}/${path}`);
+    cy.get('#burger-menu')
+        .find(`a[href*="/${locale}/${path}"]`)
+        .should('have.css', 'text-decoration-line', 'underline')
+};
 
 describe('Navigation', () => {
+    beforeEach(() => {
+        cy.clearCookies();
+    });
+
     it('should navigate to Home page defaulted to locale en', () => {
-        visitHomePageLocaleEn();
+        visitHomePageWithLocale(locales.en);
     });
 
     it('should navigate to Work History Page', () => {
-        visitHomePageLocaleEn();
-        openNavMenuLocaleEn();
-        findAndClickNavLink('en/work-history')
+        visitHomePageWithLocale(locales.en);
+        openNavMenuWithLocale(locales.en);
+        findAndClickNavLink(locales.en, 'work-history', 'Work History');
     });
 
     it('should navigate to Projects Page', () => {
-        visitHomePageLocaleEn();
-        openNavMenuLocaleEn();
-        findAndClickNavLink('en/projects');
+        visitHomePageWithLocale(locales.en);
+        openNavMenuWithLocale(locales.en);
+        findAndClickNavLink(locales.en, 'projects', 'Projects');
     });
 
     it('should navigate to Contact Page', () => {
-        visitHomePageLocaleEn();
-        openNavMenuLocaleEn();
-        findAndClickNavLink('en/contact');
+        visitHomePageWithLocale(locales.en);
+        openNavMenuWithLocale(locales.en);
+        findAndClickNavLink(locales.en, 'contact', 'Email me');
+    });
+
+    describe('when locale is ja', () => {
+        beforeEach(() => {
+            cy.setCookie('NEXT_LOCALE', locales.ja);
+        });
+
+        it('should navigate to Home page defaulted to locale ja', () => {
+            visitHomePageWithLocale(locales.ja);
+        });
+
+        it('should navigate to Work History Page', () => {
+            visitHomePageWithLocale(locales.ja);
+            openNavMenuWithLocale(locales.ja);
+            findAndClickNavLink(locales.ja, 'work-history', 'Work History');
+        });
+
+        it('should navigate to Projects Page', () => {
+            visitHomePageWithLocale(locales.ja);
+            openNavMenuWithLocale(locales.ja);
+            findAndClickNavLink(locales.ja, 'projects', 'Projects');
+        });
+
+        it('should navigate to Contact Page', () => {
+            visitHomePageWithLocale(locales.ja);
+            openNavMenuWithLocale(locales.ja);
+            findAndClickNavLink(locales.ja, 'contact', 'Email me');
+        });
     });
 });
