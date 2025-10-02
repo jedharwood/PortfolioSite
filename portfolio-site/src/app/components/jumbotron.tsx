dd29c284895199c9bc2ab854cb198eb7a6512599
Jed Harwood <scaryteeth@hotmail.com>
2025-10-02 20:25:43 +0100

    Implementing themes selections

--- portfolio-site/src/app/components/jumbotron.tsx
+++ portfolio-site/src/app/components/jumbotron.tsx
@@ -4,30 +4,35 @@ import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import gotokuji from '../../../public/jumbotron/gotokuji-cats.webp';
 import hakkei from '../../../public/jumbotron/hakkei-lanterns.webp';
+import { useTheme } from '../theme-provider';
 
 const Jumbotron = (): JSX.Element | null => {
     const t = useTranslations('Components.jumbotron');
+    const { theme } = useTheme();
 
     return (
         <div className='relative aspect-[2762/911] w-full overflow-hidden'>
-            <Image
-                src={gotokuji}
-                alt={t('gotokuji')}
-                className='object-cover shadow-lg dark:hidden'
-                placeholder='blur'
-                priority
-                fill
-                sizes='100vw'
-            />
-            <Image
-                src={hakkei}
-                alt={t('hakkei')}
-                className='hidden object-cover shadow-lg dark:block'
-                placeholder='blur'
-                priority
-                fill
-                sizes='100vw'
-            />
+            {theme === 'light' ? (
+                <Image
+                    src={gotokuji}
+                    alt={t('gotokuji')}
+                    className='object-cover shadow-lg'
+                    placeholder='blur'
+                    priority
+                    fill
+                    sizes='100vw'
+                />
+            ) : (
+                <Image
+                    src={hakkei}
+                    alt={t('hakkei')}
+                    className='object-cover shadow-lg'
+                    placeholder='blur'
+                    priority
+                    fill
+                    sizes='100vw'
+                />
+            )}
         </div>
     );
 };
